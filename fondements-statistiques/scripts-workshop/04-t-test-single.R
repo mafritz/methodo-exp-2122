b06da0a6cdf3b26a497a5b3283f053c7e3777c90
Mattia A. Fritz <mattia.fritz@unige.ch>
2022-03-29 23:24:15 +0200

    Single t-test

---
+++ fondements-statistiques/scripts-workshop/04-t-test-single.R
@@ -0,0 +1,71 @@
+set.seed(8923)
+library(tidyverse)
+library(see)
+library(effectsize)
+library(report)
+theme_set(theme_modern())
+
+
+# Test inférentiel pour comparer deux groupes indépendants ----------------
+
+# Paramètres du macro-monde (que nous ne connaissons normalement pas !)
+moyenne_groupe_A <- 100
+ecart_type_groupe_A <- 40
+
+moyenne_groupe_B <- 115
+ecart_type_groupe_B <- 40
+
+# Paramètres du micro-monde
+n_participants_per_groupe <- 20
+
+# Génération données groupe A
+data_groupe_A <- tibble(
+  groupe = "A",
+  mesure = rnorm(n_participants_per_groupe, mean = moyenne_groupe_A, sd = ecart_type_groupe_A)
+)
+
+# Génération données groupe B
+data_groupe_B <- tibble(
+  groupe = "B",
+  mesure = rnorm(n_participants_per_groupe, mean = moyenne_groupe_B, sd = ecart_type_groupe_B)
+)
+
+# Mettre les deux groupes dans le même jeu de données
+data_combined <- bind_rows(data_groupe_A, data_groupe_B)
+
+# Montrer graphiquement les résultats
+ggplot(data = data_combined, aes(x = groupe, y = mesure, color = groupe)) +
+  geom_jitter(alpha = 0.2) +
+  stat_summary(
+    fun.data = mean_cl_normal,
+    geom = "errorbar",
+    width = 0.3,
+    position = position_dodge(width = 0.1)
+  ) +
+  stat_summary(
+    fun = mean, geom = "point",
+    size = 3,
+    shape = 15,
+    position = position_dodge(width = 0.6)
+  ) +
+  labs(
+    x = "Modalité de la seule VI",
+    y = "Mesure", title = "Graphique du test. Barres = CI 95%"
+  ) +
+  scale_color_flat() +
+  theme(legend.position = "none")
+
+# Effectuer un t-test de Welch avec hétérogénité de la variance
+model <- t.test(
+  formula = mesure ~ groupe,
+  data = data_combined,
+  alternative = "two.sided",
+  paired = FALSE,
+  var.equal = FALSE
+)
+
+# Afficher les résultats "brutes"
+print(model)
+
+# Afficher une synthèse textuelle (ignorer le warning)
+report(model)
